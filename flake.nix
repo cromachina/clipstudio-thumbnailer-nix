efb0b7d608086dfddf75402c33abc8008cfb6af0
cromachina <82557197+cromachina@users.noreply.github.com>
2026-05-07 22:33:46 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      script = "clipstudio-thumbnailer.sh";
+      deps = with pkgs; [ sqlite coreutils gnugrep bash ];
+      clipstudio-thumbnailer-lib = pkgs.stdenv.mkDerivation {
+        name = "clipstudio-thumbnailer-lib";
+        src = pkgs.fetchFromGitHub {
+          owner = "ludospex";
+          repo = "clipstudio-thumbnailer";
+          rev = "3bb5cfe5a426eb2a725abf32b44e75885a306240";
+          hash = "sha256-u3lahWBz8f11abnKYaqwjgJZ9zte9tTV7rPVpoeaBJw=";
+        };
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ${script} $out/bin/.${script}-bash-wrapped.sh
+          echo "#! /usr/bin/env bash
+          bash $out/bin/.${script}-bash-wrapped.sh \$@" > $out/bin/${script}
+          chmod a+x $out/bin/${script}
+          wrapProgram $out/bin/${script} --prefix PATH : ${pkgs.lib.makeBinPath deps}
+        '';
+      };
+      thumbnailer = pkgs.writeTextFile {
+        name = "clipstudio-thumbnailer";
+        destination = "/share/thumbnailers/clipstudio.thumbnailer";
+        text = ''
+          [Thumbnailer Entry]
+          Exec=${clipstudio-thumbnailer-lib}/bin/${script} %i %o
+          MimeType=application/x-clip;image/x-clip;application/octet-stream;application/x-wine-extension-clip;
+        '';
+      };
+      in {
+        packages.default = thumbnailer;
+      }
+  );
+}
